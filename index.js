ae80bd46cceb1956077655569a555565d20cfa28
Lorcan McDonald <lorcan@lorcanmcdonald.com>
2015-06-01 15:26:49 +0100

    Normalize path
    Normalizing the path here will prevent getting access to files outside
    of the current working directory

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 
 var fs   = require('fs'),
+    path = require('path'),
     url  = require('url'),
     templates = require('./templates'),
     Server = require('./server').Server;
 
 Server.new(function (req, res) {
-    console.log('req.url', req.url);
+    console.log(req.url);
     var parsedUrl = url.parse(req.url, true);
-    var pathname = parsedUrl.pathname;
+    var pathname = path.normalize(parsedUrl.pathname);
 
     fs.stat('.' + pathname, function (err, stats) {
         if(err && err.code === "ENOENT") {
@@ -18,11 +19,11 @@ Server.new(function (req, res) {
         } else if(stats.isDirectory()) {
             res.writeHead(200, Server.contentTypes['text/html']);
 
-            fs.readdir('.' +pathname, function(err, files) {
+            fs.readdir('.' + pathname, function(err, files) {
                 if (err) {
                     Server.serverError(res, err);
                 }
-                var baseUrl = pathname.match(/^\/$/)? '': pathname+'/';
+                var baseUrl = pathname.match(/^\/$/)? '': pathname + '/';
                 res.end(templates.main({baseUrl: baseUrl, files: files}));
             });
         } else {
