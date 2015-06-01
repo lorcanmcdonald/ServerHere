4e476cd03410f25147f218c9eac5d2f552ee0f74
Lorcan McDonald <lorcan@lorcanmcdonald.com>
2015-06-01 15:13:18 +0100

    Avoid repeated redirection

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ var fs   = require('fs'),
 Server.new(function (req, res) {
     console.log('req.url', req.url);
     var parsedUrl = url.parse(req.url, true);
+    var pathname = parsedUrl.pathname;
     var contentType = 'text/plain';
-    if(parsedUrl.pathname === '/' || parsedUrl.pathname.match(/html$/)) {
+    if(pathname === '/' || pathname.match(/html$/)) {
         contentType = 'text/html';
-    } else if(parsedUrl.pathname.match(/js$/)) {
+    } else if(pathname.match(/js$/)) {
         contentType = 'text/javascript';
     }
 
-    fs.stat('.' + parsedUrl.pathname, function (err, stats) {
+    fs.stat('.' + pathname, function (err, stats) {
         if(err && err.code === "ENOENT") {
             Server.notFound(res);
         } else if (err) {
@@ -23,28 +24,28 @@ Server.new(function (req, res) {
         } else if(stats.isDirectory()) {
             res.writeHead(200, Server.contentTypes['text/html']);
 
-            fs.readdir('.' +parsedUrl.pathname, function(err, files) {
+            fs.readdir('.' +pathname, function(err, files) {
                 if (err) {
                     Server.serverError(res, err);
                 }
-                var baseUrl = parsedUrl.pathname.match(/^\/$/)? '': parsedUrl.pathname+'/';
+                var baseUrl = pathname.match(/^\/$/)? '': pathname+'/';
                 res.end(templates.main({baseUrl: baseUrl, files: files}));
             });
         } else {
             contentType = 'text/plain';
-            if(parsedUrl.pathname.match(/html$/)) {
+            if(pathname.match(/html$/)) {
                 contentType = 'text/html';
-            } else if(parsedUrl.pathname.match(/\.css$/)) {
+            } else if(pathname.match(/\.css$/)) {
                 contentType = 'text/css';
-            } else if(parsedUrl.pathname.match(/\.svg$/)){
+            } else if(pathname.match(/\.svg$/)){
                 contentType = 'image/svg';
-            } else if(parsedUrl.pathname.match(/\.json$/)){
+            } else if(pathname.match(/\.json$/)){
                 contentType = 'application/json';
-            } else if(parsedUrl.pathname.match(/\.js$/)){
+            } else if(pathname.match(/\.js$/)){
                 contentType = 'text/javascript';
             }
             res.writeHead(200, Server.contentTypes[contentType]);
-            fs.readFile('.' + parsedUrl.pathname, function(err, data){
+            fs.readFile('.' + pathname, function(err, data){
                 if(err) { throw err; }
                 res.end(data);
             });
